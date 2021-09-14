f7158d3f24a56ee1b2baeab08c8001d7924003b8
KaosBunny <Niamh.K.Ferns@gmail.com>
2021-09-15 11:20:45 +1200

    Main work on assignment 2 is done. Time to debug.

--- 159102/assignment-2/main.cpp
+++ 159102/assignment-2/main.cpp
@@ -1,23 +1,20 @@
-#include <cctype>
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <sstream>
 
 // globals
 // pc = program counter, ir = instuction register.
-int memory[256] = {0}, reg[16], pc, ir;
+int memory[256] = {0}, reg[16], pc = 0, ir;
+bool halted = 0;
 
 // FUNCTIONS
 
 // Go through and store all the instuctions from the txt file and move all the
 // various instruction nums to memory.
 void loadInstructions(std::string instuctionsPath);
-void decodeInstruction();
-void executeInstruction();
-
-// Print the current instuction being completed.
-void printInstruction(/* if any other params are needed I'm not sure yet... */);
+void fetchInstruction();
+void decodeInstruction(int cache[4]);
+void executeInstruction(int cache[4]);
 
 int main() {
     std::string userInput = "";
@@ -26,12 +23,13 @@ int main() {
 
     // Boot up the MASSEY machine.
     loadInstructions(userInput);
-
-    // Main program loop.
-
-    // Remember... fetch, decode, execute.
-    executeInstruction();
-    ++pc;
+    while (!halted && pc < 256) {
+        int cache[4] = {0};
+        fetchInstruction();
+        decodeInstruction(cache);
+        executeInstruction(cache);
+        ++pc;
+    }
 
     return 0;
 }
@@ -50,7 +48,7 @@ void loadInstructions(std::string instuctionsPath) {
     while (std::getline(commands, currentLine)) {
         // to load into memory we need to convert each char from hex to binary
         for (int i = 0; i < 4; ++i) { // this will break without more checks but the notes said to not validate input so...
-            memory[instuction] = memory[instuction] << 8; // All instuctions are seperated by a
+            memory[instuction] = memory[instuction] << 4; // All instuctions are seperated by a
             if (currentLine[i] <= '9') {
                 memory[instuction] += currentLine[i] - 48;
                 continue;
@@ -65,6 +63,92 @@ void loadInstructions(std::string instuctionsPath) {
     commands.close();
 }
 
-void printInstruction() {
+void fetchInstruction() {
+    ir = memory[pc];
+    ++pc;
+}
 
+void decodeInstruction(int cache[4]) {
+    // NOTE: The part is 0 indexed. EG: In 3RXY to access the register we need part = 1.
+    for (int i = 0; i < 4; ++i) {
+        int mask = (0xF000 >> i * 4);
+        cache[i] = (ir & mask) >> ((3 - i) * 4);
+    }
+}
+
+void executeInstruction(int cache[4]) {
+    std::cout << std::hex << ir;
+    switch (cache[0]) {
+        case 1:
+            reg[cache[1]] = (cache[2] << 4) + cache[3];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 2:
+            reg[cache[2]] = reg[cache[3]];
+            std::cout << std::hex << "    R" << cache[2] << " = " << reg[cache[2]];
+            break;
+
+        case 3:
+            reg[cache[1]] = memory[(cache[2] << 4) + cache[3]];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 4:
+            memory[(cache[2] << 4) + cache[3]] = reg[cache[1]];
+            std::cout << std::hex << "    Memory[" << (cache[2] << 4) + cache[3] << "] = " << reg[cache[1]];
+            break;
+
+        case 6:
+            reg[cache[1]] = reg[cache[2]] + reg[cache[3]];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 7:
+            reg[cache[2]] = reg[cache[2]] * -1;
+            std::cout << std::hex << "    R" << cache[2] << " = " << reg[cache[2]];
+            break;
+
+        case 8:
+            reg[cache[1]] = reg[cache[1]] << cache[3];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 9:
+            reg[cache[1]] = reg[cache[1]] >> cache[3];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 0xA:
+            reg[cache[1]] = reg[cache[2]] & reg[cache[3]];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 0xB:
+            reg[cache[1]] = reg[cache[2]] | reg[cache[3]];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 0xC:
+            reg[cache[1]] = reg[cache[2]] ^ reg[cache[3]];
+            std::cout << std::hex << "    R" << cache[1] << " = " << reg[cache[1]];
+            break;
+
+        case 0xD:
+            if (reg[cache[1]] == reg[0]) {
+                pc = (cache[2] << 4) + cache[3];
+                std::cout << "    R" << cache[1] << "== R0 : JUMP";
+            }
+            std::cout << "    R" << cache[1] << "!= R0 : NO JUMP";
+            break;
+
+        case 0xE:
+            halted = 1;
+            std::cout << "Halt\n";
+            break;
+
+        default:
+            std::cout << "    This is not a valid instruction... \n";
+    }
+    if (!halted) std::cout << std::hex << "    PC = " << pc << std::endl;
 }
