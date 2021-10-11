edb8ff05ee4f214dc51667b90e034d8f64f8fd67
KaosBunny <Niamh.K.Ferns@gmail.com>
2021-10-11 18:22:27 +1300

    Finished final assignment for Technical Programming 2.

--- 159102/assignment-3/main.cpp
+++ 159102/assignment-3/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
+
 #include <string>
 #include <vector>
-#include <cmath>
 
+#include <cmath>
+#include <cstdlib>
+
+// I write these out a lot so having macros makes it easier to read.
 #define thisPixel picture[x][y]
+#define thisButton buttons[i]
 
 using namespace std;
-
 
 class pixel_class {
 private:
@@ -24,25 +27,11 @@ public:
     bool getexclude() { return exclude; }
 };
 
-void loadButtons();
-void scanImage();
-void findArea(int x, int y, bool invert);
-void findDamaged();
-void drawBoxes();
-void saveImage();
-
-int maxDeviation = 5;  // MUST be global if used
-int screenx, screeny, maxcolours;   // you must use these
-pixel_class picture[600][600];      // you must use thism
-// Index Key
-// 0 - Xmax    1 - Xmin
-// 2 - Ymax    3 - Ymin
-// 4 - pixels  5 - broken
-struct circlePosition {
+struct button {
     int top, bottom, left, right, count;
     bool damaged;
 
-    circlePosition(int top, int bottom, int left, int right, int count, bool damaged) {
+    button(int top, int bottom, int left, int right, int count, bool damaged) {
         this->top = top;
         this->bottom = bottom;
         this->left = left;
@@ -52,7 +41,20 @@ struct circlePosition {
     }
 };
 
-vector<circlePosition> circles; // These are the globals from above just... not in an annoying way.
+void loadButtons();
+void scanImage();
+void findArea(int x, int y, bool invert);
+void findDamaged();
+void drawBoxes();
+void saveImage();
+
+int screenx, screeny, maxcolours;   // you must use these
+pixel_class picture[600][600];      // you must use thism
+
+// This is hardcoded now but if this were to be expanded, you would set this value with a calibration function.
+int maxDeviation = 5;
+
+vector<button> buttons; // These are the globals from above just... not in an annoying way.
 
 int main() {
 
@@ -65,118 +67,6 @@ int main() {
 
     // Step 3 : output the final .ppm file
     saveImage();
-}
-
-void scanImage() {
-    for (int y = 0; y < 600; ++y) {
-        for (int x = 0; x < 600; ++x) {
-            // Check if we should ignore this pixel.
-            if (thisPixel.getexclude()) continue;
-            if (thisPixel.getR() > 128 || thisPixel.getG() > 128 || thisPixel.getB() > 128) {
-                circles.push_back(circlePosition(y, y, x, x, 1, 0));
-                findArea(x, y, 0); // Enter recusrive find function.
-            }
-        }
-    }
-
-    findDamaged();
-}
-
-void findArea(int x, int y, bool invert) {
-    // Base Cases
-    if (thisPixel.getexclude()) return;
-    if (!invert) {
-        if (thisPixel.getR() <= 128 && thisPixel.getG() <= 128 && thisPixel.getB() <= 128) return;
-        circles.back().count++; // Increase pixel count for nth circle.
-        if      (y < circles.back().top)    circles.back().top    = y;
-        else if (y > circles.back().bottom) circles.back().bottom = y;
-        else if (x < circles.back().left)   circles.back().left   = x;
-        else if (x > circles.back().right)  circles.back().right  = x;
-    }
-    else {
-        if (thisPixel.getR() > 128 || thisPixel.getG() > 128 || thisPixel.getB() > 128) return;
-    }
-    thisPixel.setexclude(true);
-
-    findArea(x + 1, y    , invert);
-    findArea(x - 1, y    , invert);
-    findArea(x    , y + 1, invert);
-    findArea(x    , y - 1, invert);
-}
-
-void findDamaged() {
-    // So uh... I wanted to do something dumb and try and be a smartass. It kinda worked..? I don't have time to make more images to check
-    // but yeah. c:
-    //
-    // I'd really have liked to find a way to check the edge pixels of the circle against their distance from the centre to determine
-    // whether they are not a circle and thus are damaged, but I couldn't figure out how.
-    // So, I just drew a circle with a maxDeviation distance from the edge and checked each pixel on that circle. I'd really like feedback
-    // on how I could have made this work the otherway.
-
-    for (int i = 0; i < circles.size(); ++i) {
-        int width = (circles[i].right - circles[i].left);
-        int height = (circles[i].bottom - circles[i].top);
-        int deviation = abs(width - height), radius;
-
-        if (width > height) radius = height / 2 - maxDeviation;
-        else radius = width / 2 - maxDeviation;
-
-        for (int theta = 0, x, y; theta <= 360; theta += 10) {
-            x = radius * cos(theta) + circles[i].left + width / 2;
-            y = radius * sin(theta) + circles[i].top + height / 2;
-
-            if (thisPixel.getR() <= 128 && thisPixel.getG() <= 128 && thisPixel.getB() <= 128) circles[i].damaged = true;
-        }
-
-        if (circles[i].damaged) continue;
-
-         radius = 15;
-         circles[i].count = 0;
-         for (int theta = 0, x, y; theta <= 360; theta += 10) {
-             x = radius * cos(theta) + circles[i].left + width / 2;
-             y = radius * sin(theta) + circles[i].top + height / 2;
-
-             if (thisPixel.getR() <= 128 && thisPixel.getG() <= 128 && thisPixel.getB() <= 128 && !thisPixel.getexclude()) {
-                 findArea(x, y, 1);
-                 circles[i].count++;
-             }
-         }
-        if (circles[i].count < 4) circles[i].damaged = true;
-    }
-}
-
-void drawBoxes() {
-    for (int i = 0; i < circles.size(); ++i) {
-        for (int y = circles[i].top; y <= circles[i].bottom; ++y) {
-            if(!circles[i].damaged) picture[circles[i].left][y].loaddata(0, 0xFF, 0);
-            else picture[circles[i].left][y].loaddata(0xFF, 0, 0);
-
-            for (int x = circles[i].left; x <= circles[i].right; ++x) {
-                if (y != circles[i].top && y != circles[i].bottom) continue; // This is disgusting, I'm so sorry...
-                if(!circles[i].damaged) thisPixel.loaddata(0, 0xFF, 0);
-                else thisPixel.loaddata(0xFF, 0, 0);
-            }
-
-            if(!circles[i].damaged) picture[circles[i].right][y].loaddata(0, 0xFF, 0);
-            else picture[circles[i].right][y].loaddata(0xFF, 0, 0);
-        }
-    }
-}
-
-void saveImage() {
-    fstream outfile;
-    string filename;
-    cout << "What would you like to save the file as? ";
-    getline(cin, filename);
-
-    outfile.open(filename.c_str(), fstream::out);
-    outfile << "P3\n# " << filename << endl << screenx << " " << screeny << endl << maxcolours << endl;
-    for (int y = 0; y < screeny; ++y) {
-        for (int x = 0; x < screenx; x++) {
-            outfile << thisPixel.getR() << " " << thisPixel.getG() << " " << thisPixel.getB() << " ";
-        }
-        outfile << endl;
-    }
 }
 
 void loadButtons() {
@@ -202,6 +92,133 @@ void loadButtons() {
         }
     }
     infile.close();
+}
+
+void scanImage() {
+    for (int y = 0; y < 600; ++y) {
+        for (int x = 0; x < 600; ++x) {
+            // Check if we should ignore this pixel.
+            if (thisPixel.getexclude()) continue;
+
+            // If we find a pixel, use it as a seed to find the rest.
+            if (thisPixel.getR() > 128 || thisPixel.getG() > 128 || thisPixel.getB() > 128) {
+                buttons.push_back(button(y, y, x, x, 1, 0));
+                findArea(x, y, 0); // Enter recusrive find function.
+            }
+        }
+    }
+
+    findDamaged();
+}
+
+// Genereal purpose function to find the area of a cirle or gap.
+void findArea(int x, int y, bool invert) {
+    // Base Cases
+    if (thisPixel.getexclude()) return;
+    if (!invert) {
+        // Base Case A
+        if (thisPixel.getR() <= 128 && thisPixel.getG() <= 128 && thisPixel.getB() <= 128) return;
+
+        if      (y < buttons.back().top)    buttons.back().top    = y;
+        else if (y > buttons.back().bottom) buttons.back().bottom = y;
+        if      (x < buttons.back().left)   buttons.back().left   = x;
+        else if (x > buttons.back().right)  buttons.back().right  = x;
+    }
+    else {
+        // Base Case B
+        if (thisPixel.getR() > 128 || thisPixel.getG() > 128 || thisPixel.getB() > 128) return;
+    }
+    thisPixel.setexclude(true);
+
+    findArea(x + 1, y    , invert);
+    findArea(x - 1, y    , invert);
+    findArea(x    , y + 1, invert);
+    findArea(x    , y - 1, invert);
+}
+
+// First note that a key feature of a circle is having a consistent radius at every angle.
+// Given a list of cordinates for a circle (maxX, maxY, minX minY), find a radius, the centre point,
+// and then use these to determine whether the button is perfectly circular or missing bits by
+// checking whether radius at angle n is still a pixel with an R/G/B value > 128.
+// We do it this way because just checking total pixel count has greater chance for error.
+void findDamaged() {
+    // So uh... I wanted to do something dumb and try and be a smartass. It kinda worked..? I don't have time to make more images to check
+    // but yeah. c:
+    //
+    // I'd really have liked to find a way to check the edge pixels of the circle against their distance from the centre to determine
+    // whether they are not a circle and thus are damaged, but I couldn't figure out how.
+    // So, I just drew a circle with a maxDeviation distance from the edge and checked each pixel on that circle. I'd really like feedback
+    // on how I could have made this work the otherway.
+    for (int i = 0; i < buttons.size(); ++i) {
+        // Get our width and height.
+        int width = (thisButton.right - thisButton.left);
+        int height = (thisButton.bottom - thisButton.top);
+
+        // We need the radius to be the max() - the maxDeviation.
+        // That is... that is the radius around the circle at which we would consider any pixels < 128 to be over the maximum allowed
+        // varience for a circle. It also accounts for if the difference of the height and width being greater than maxDeviation.
+        // We use an outer radius to check if it's circular, then and inner radius to check whether there are 4 holes.
+        int oRadius = max(width, height) / 2 - maxDeviation, iRadius = 15;
+        thisButton.count = 0;
+        for (int theta = 0, x, y; theta <= 360; theta += 10) {
+            // Get (X, Y) for each radius pixel then check it's value.
+            x = oRadius * cos(theta) + thisButton.left + width / 2;
+            y = oRadius * sin(theta) + thisButton.top + height / 2;
+            if (thisPixel.getR() < 128 && thisPixel.getG() < 128 && thisPixel.getB() < 128) {
+                thisButton.damaged = true;
+                break;
+            }
+
+            // Get (X, Y) for each inner radius pixel then check whether it's part of a hole.
+            x = iRadius * cos(theta) + thisButton.left + width / 2;
+            y = iRadius * sin(theta) + thisButton.top + height / 2;
+            if (thisPixel.getR() <= 128 && thisPixel.getG() <= 128 && thisPixel.getB() <= 128 && !thisPixel.getexclude()) {
+                findArea(x, y, 1);
+                thisButton.count++;
+            }
+        }
+        if (thisButton.count < 4) thisButton.damaged = true;
+    }
+}
+
+void drawBoxes() {
+    // Is this a pointer to circles[index] or is it copying the value of circles[index] into a seperate entity of circlePosition?
+    // I wanted circle to just pe a pointer so it's easier to read later on and less copying to do.
+    for (int i = 0; i < buttons.size(); ++i) {
+        for (int y = thisButton.top; y <= thisButton.bottom; ++y) {
+            // Draw our left side pixels.
+            if(!thisButton.damaged) picture[thisButton.left][y].loaddata(0, 0xFF, 0);
+            else picture[thisButton.left][y].loaddata(0xFF, 0, 0);
+
+            // Draw our horizontal pixels
+            if (y == thisButton.top || y == thisButton.bottom) {
+                for (int x = thisButton.left; x <= thisButton.right; ++x) {
+                    if(!thisButton.damaged) thisPixel.loaddata(0, 0xFF, 0);
+                    else thisPixel.loaddata(0xFF, 0, 0);
+                }
+            }
+
+            // Draw the right side pixels.
+            if(!thisButton.damaged) picture[thisButton.right][y].loaddata(0, 0xFF, 0);
+            else picture[thisButton.right][y].loaddata(0xFF, 0, 0);
+        }
+    }
+}
+
+void saveImage() {
+    fstream outfile;
+    string filename;
+    cout << "What would you like to save the file as? ";
+    getline(cin, filename);
+
+    outfile.open(filename.c_str(), fstream::out);
+    outfile << "P3\n# " << filename << endl << screenx << " " << screeny << endl << maxcolours << endl;
+    for (int y = 0; y < screeny; ++y) {
+        for (int x = 0; x < screenx; x++) {
+            outfile << thisPixel.getR() << " " << thisPixel.getG() << " " << thisPixel.getB() << " ";
+        }
+        outfile << endl;
+    }
 }
 
 //--------------- methods for the pixel_class ------------
