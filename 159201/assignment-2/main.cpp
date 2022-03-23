e15ea02df6d2ee870da4caec81056f2ff6a0c3f1
KaosBunny <Niamh.K.Ferns@gmail.com>
2022-03-24 04:30:34 +1300

    Outlining for assignment 2.

--- 159201/assignment-2/main.cpp
+++ 159201/assignment-2/main.cpp
@@ -1,6 +1,36 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Stack {
+
+};
+
+void run_operation(char c, Stack & s);
+
 int main(int argc, char** argv) {
-    // Check that you have a filename as an argument.
-    // Open a file.
-    // Loop through all the lines in the file.
+
+    if (argc == 1) {
+        std::cout << "Please provide a file argument..." << std::endl;
+        exit(0);
+    }
+
+    std::ifstream file;
+    file.open(argv[1]);
+    if (!file.good()) {
+        std::cout << "Unfortunately, your file failed to open." << std::endl;
+        exit(0);
+    }
+
+    std::string s;
+    while(!file.eof()) {
+        getline(file, s);
+        if (isdigit(s[0])) {
+            // expression.push(s) you'll need a try catch here for if it's not actually an int...
+            continue;
+        }
+        // run_operation(s[0], expression);
+    }
     return 0;
 }
