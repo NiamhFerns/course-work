22223d811bf283c1bc0c6821820b66b987bf398b
KaosBunny <Niamh.K.Ferns@gmail.com>
2022-03-31 15:35:46 +1300

    Testing the game engine thingo from my course.

---
+++ 159261/MyFirstGame/src/Test.java
@@ -0,0 +1,19 @@
+public class Test /* extends GameEngine */ {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    /*
+    public static void main(String[] args) {
+        Test game = new Test();
+        createGame(game, 30);
+    }
+
+    public void update(double fps) {
+        System.out.println("30");
+    }
+
+    public void paintComponent() {
+
+    }
+     */
+}
