5701ea36bc5151cf375c53a5b0ae6d31bbfee91b
KaosBunny <Niamh.K.Ferns@gmail.com>
2023-03-27 11:07:28 +1300

    Finished NWEN241 assignment 1.

--- NWEN-241/assignment-1/files/editor.c
+++ NWEN-241/assignment-1/files/editor.c
@@ -30,7 +30,6 @@ int editor_delete_char(char editing_buffer[], int editing_buflen,
     if (editing_buflen <= 0 || offset < 0 || offset > editing_buflen - 1)
         return 0;
 
-
     bool found = 0;
 
     // Find the correct index of the character to remove if it's not already correct.
@@ -84,7 +83,8 @@ int editor_replace_str(char editing_buffer[], int editing_buflen,
         }
     }
 
-    if (!in_word) return -1;
+    if (!in_word)
+        return -1;
 
     // Shift characters to the left.
     if (str_len > rep_len) {
@@ -124,7 +124,28 @@ int editor_replace_str(char editing_buffer[], int editing_buflen,
 void editor_view(int rows, int cols, char viewing_buffer[rows][cols],
     const char editing_buffer[], int editing_buflen, int wrap)
 {
-    printf("Unimplemented");
+    // Clear the viewing buffer.
+    for (int y = 0; y < rows; ++y) {
+        for (int x = 0; x < cols; ++x) {
+            viewing_buffer[y][x] = '\0';
+        }
+    }
+
+    // Insert char at the current location.
+    int pos = 0;
+    for (int y = 0; y < rows && pos < editing_buflen - 1; ++y) {
+        for (int x = 0; (!wrap || x < cols - 1) && pos < editing_buflen - 1; ++x, ++pos) {
+            // If we find a newline, just break and continue on the line and skip the character insertion part.
+            if (editing_buffer[pos] == '\n') {
+                ++pos;
+                break;
+            }
+            if (x >= cols - 1) continue;
+            if (editing_buffer[pos] == '\n') return;
+
+            viewing_buffer[y][x] = editing_buffer[pos];
+        }
+    }
 }
 
 // int main (int argc, char *argv[])
@@ -133,6 +154,6 @@ void editor_view(int rows, int cols, char viewing_buffer[rows][cols],
 //     strcpy(editing_buffer, "The quick brown fox");
 //
 //     editor_replace_str(editing_buffer, 21, "brown", "blue", 0);
-//     
+//
 //     return 0;
 // }
