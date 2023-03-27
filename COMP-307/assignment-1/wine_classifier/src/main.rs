59e6f9f14ceac81b67cd0ba9fa16b41727265070
KaosBunny <Niamh.K.Ferns@gmail.com>
2023-03-28 10:51:23 +1300

    Begun work on COMP307 A1 P1.

--- COMP-307/assignment-1/wine_classifier/src/main.rs
+++ COMP-307/assignment-1/wine_classifier/src/main.rs
@@ -1,3 +1,60 @@
+use std::{env::args, fs};
+
+#[allow(dead_code)]
+mod knn_model {
+    use std::fs;
+
+    #[derive(Default)]
+    pub struct Instance {
+        alcohol: i32,
+        malic_acid: i32,
+        ash: i32,
+        alcalinity_of_ash: i32,
+        magnesium: i32,
+        total_phenols: i32,
+        flavanoids: i32,
+        nonflavanoid_phenols: i32,
+        proantheocyanins: i32,
+        colour_intensity: i32,
+        hue: i32,
+        od_value: i32,
+        proline: i32,
+    }
+
+    pub struct Model {
+        k_value: i32,
+        training_data: Vec<Instance>,
+        testing_data: Vec<Instance>,
+    }
+
+    impl Model {
+        pub fn train(training: &str, testing: &str, k_value: i32) -> Model {
+            let training_data =
+                fs::read_to_string(training).expect("Failed to read in file for training values.");
+            let testing_data =
+                fs::read_to_string(testing).expect("Failed to read in file for training values.");
+
+            let training_data: Vec<_> = training_data.split("\n").collect();
+            let testing_data: Vec<_> = testing_data.split("\n").collect();
+
+            Model {
+                k_value,
+                training_data: vec![Instance {
+                    ..Default::default()
+                }],
+                testing_data: vec![Instance {
+                    ..Default::default()
+                }],
+            }
+        }
+
+        pub fn test(&self) {}
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = args().collect();
+    let model = knn_model::Model::train(&args[1], &args[2], 3);
+
+    model.test();
 }
---
+++ COMP-307/assignment-1/wine_classifier/target/debug/deps/wine_classifier-2fdc8b234b23c58d.d
@@ -0,0 +1,5 @@
+/home/niamh/Repos/course-work/COMP-307/assignment-1/wine_classifier/target/debug/deps/wine_classifier-2fdc8b234b23c58d: src/main.rs
+
+/home/niamh/Repos/course-work/COMP-307/assignment-1/wine_classifier/target/debug/deps/wine_classifier-2fdc8b234b23c58d.d: src/main.rs
+
+src/main.rs:
