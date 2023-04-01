671d7b0ba31c5a6e25a696f6c4b5914750244338
KaosBunny <Niamh.K.Ferns@gmail.com>
2023-04-01 17:10:38 +1300

    .

--- NWEN-241/exercise-2/activity2.c
+++ NWEN-241/exercise-2/activity2.c
@@ -10,13 +10,12 @@ void print_record(struct record rec) {
     printf("Name  : %s\nAge   : %d\nHeight: %.2f", rec.name, rec.age, rec.height);
 }
 
-
 int main (int argc, char *argv[])
 {
     struct record rec;
 
     scanf("%s %d %f", rec.name, &rec.age, &rec.height);
     print_record(rec);
-
+    
     return 0;
 }
