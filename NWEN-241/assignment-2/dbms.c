d643ba410a38639c05ab8d667a8b1d3b7ea31360
KaosBunny <Niamh.K.Ferns@gmail.com>
2023-04-17 13:43:35 +1200

    Commented task 3.

--- NWEN-241/assignment-2/dbms.c
+++ NWEN-241/assignment-2/dbms.c
@@ -10,7 +10,7 @@ int db_show_row(const struct db_table* db, unsigned int row)
     if (row > db->rows_used)
         return 0;
 
-    struct album *instance = &db->table[row];
+    struct album* instance = &db->table[row];
 
     printf("%6lu:%20.20s:%20.20s:%-4.4d\n", instance->id, instance->title, instance->artist, instance->year);
     return 1;
@@ -37,13 +37,19 @@ int db_add_row(struct db_table* db, struct album* a)
  * Function prototype for Task 3. See handout for details.
  */
 int db_remove_row(struct db_table* db, unsigned long id)
-{   
+{
+    // Search for ID.
     int i;
     for (i = 0; i < db->rows_used; ++i) {
-        if (db->table[i].id == id) break;
+        if (db->table[i].id == id)
+            break;
     }
-    if (i >= db->rows_used) return 0;
 
+    // ID doesn't exist in this table.
+    if (i >= db->rows_used)
+        return 0;
+
+    // If the table is now empty, just free it and early exit.
     db->rows_used--;
     if (db->rows_used <= 0) {
         db->rows_total = 0;
@@ -52,10 +58,12 @@ int db_remove_row(struct db_table* db, unsigned long id)
         return 1;
     }
 
+    // Move all elements down 1.
     for (int j = i; j < db->rows_used; j++) {
         db->table[j] = db->table[j + 1];
     }
 
+    // Free unneeded memory if needed.
     if (db->rows_total - db->rows_used >= 5) {
         db->rows_total -= 5;
         db->table = realloc(db->table, sizeof(struct album) * db->rows_total);
