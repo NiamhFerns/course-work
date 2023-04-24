8a059169c556ca025521bb6b30e867c19256f025
KaosBunny <Niamh.K.Ferns@gmail.com>
2023-04-24 14:36:48 +1200

    Added assignment 3.

---
+++ NWEN-241/assignment-3/server.c
@@ -0,0 +1,27 @@
+/**
+ * Skeleton file for server.c
+ * 
+ * You are free to modify this file to implement the server specifications
+ * as detailed in Assignment 3 handout.
+ * 
+ * As a matter of good programming habit, you should break up your imple-
+ * mentation into functions. All these functions should contained in this
+ * file as you are only allowed to submit this file.
+ */ 
+
+#include <stdio.h>
+// Include necessary header files
+
+/**
+ * The main function should be able to accept a command-line argument
+ * argv[0]: program name
+ * argv[1]: port number
+ * 
+ * Read the assignment handout for more details about the server program
+ * design specifications.
+ */ 
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
